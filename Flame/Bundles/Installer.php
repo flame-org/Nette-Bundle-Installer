1ba36b6007db2ad6b0770df71d3e72c7967e2327
JSifalda <sifalda.jiri@gmail.com>
2013-07-25 17:35:04 +0200

    Refactoring for modules usage

--- Flame/Bundles/Installer.php
+++ Flame/Bundles/Installer.php
@@ -2,25 +2,42 @@
 
 namespace Flame\Bundles;
 
+use Composer\Composer;
+use Composer\Installer\LibraryInstaller;
+use Composer\IO\IOInterface;
+use Composer\Package\PackageInterface;
+use Composer\Repository\InstalledRepositoryInterface;
+use Nette\Utils\PhpGenerator\Helpers;
+
 /**
- * Custom installer of Nette Budles
+ * Custom installer of Nette Modules
  */
-class Installer extends \Composer\Installer\LibraryInstaller
+class Installer extends LibraryInstaller
 {
-
-	/** @var array */
-	private static $supportedTypes = array(
-		'nette-bundle',
-	);
-
 	/** @var string */
 	private $appDir;
 
-	public function __construct(\Composer\IO\IOInterface $io, \Composer\Composer $composer, $type = 'library')
+	/** @var array */
+	private $supportedTypes = array(
+		'nette-module',
+	);
+
+	/** @var array  */
+	private $defaultConfigs = array(
+		'/config/extensions.php',
+		'/config/extensions.neon',
+	);
+
+	/**
+	 * @param IOInterface $io
+	 * @param Composer $composer
+	 * @param string $type
+	 */
+	public function __construct(IOInterface $io, Composer $composer, $type = 'library')
 	{
 		parent::__construct($io, $composer, $type);
 
-		$this->appDir = realpath(($this->vendorDir ? $this->vendorDir.'/' : '') . '../app');
+		$this->appDir = $this->getAppDirPath();
 	}
 
 	/**
@@ -29,50 +46,56 @@ class Installer extends \Composer\Installer\LibraryInstaller
 	 */
 	public function supports($packageType)
 	{
-		return in_array($packageType, self::$supportedTypes);
+		return in_array($packageType, $this->supportedTypes);
+	}
+
+	public function install(InstalledRepositoryInterface $repo, PackageInterface $package)
+	{
+		$this->includeExtensionClass($package);
+		parent::install($repo, $package);
 	}
 
 	/**
-	 * @param \Composer\Package\PackageInterface $package
 	 * @return string
 	 * @throws \Exception
 	 */
-	public function getInstallPath(\Composer\Package\PackageInterface $package)
+	private function getAppDirPath()
 	{
-		if($package->getType() == 'nette-bundle') {
-			$targetDir = $package->getTargetDir();
-			return $this->appDir . '/' . $this->getBundleName($package->getPrettyName()) . ($targetDir ? '/'.$targetDir : '');
-		}else{
-			throw new \Exception("Not recognized package type '{$package->getType()}'");
+		$path = realpath(($this->vendorDir ? $this->vendorDir.'/' : '') . '../app');
+		if(!file_exists($path)) {
+			$path = realpath(($this->vendorDir ? $this->vendorDir.'/' : '') . '../../app');
+			if(!file_exists($path)) {
+				throw new \Exception('We could not found "app" directory');
+			}
+		}
+
+		return $path;
+	}
+
+	/**
+	 * @param PackageInterface $package
+	 */
+	private function includeExtensionClass(PackageInterface $package)
+	{
+		$extra = $this->getExtra($package);
+		if(isset($extra['class'])) {
+			$class = $extra['class'];
+			if(file_exists($extensionsFile = $this->appDir . $this->defaultConfigs[0])) {
+				$config = include_once $extensionsFile;
+				$config[] = $class;
+				$fileContent = '<?php ' . PHP_EOL . 'return ' . Helpers::dump($config) . ';';
+				file_put_contents($extensionsFile, $fileContent);
+			}
 		}
 	}
 
 	/**
-	 * @param $prettyName
-	 * @return string
+	 * @param PackageInterface $package
+	 * @return array
 	 */
-	private function getBundleName($prettyName)
+	private function getExtra(PackageInterface $package)
 	{
-
-		if(strpos($prettyName, '/') !== false){
-			$lastOne = strrchr($prettyName, '/');
-			$name = str_replace('/', '', $lastOne);
-
-			if(strpos($name, '-') !== false){
-				$pieces = explode('-', $name);
-
-				if(count($pieces)){
-					$name = '';
-					foreach($pieces as $piece){
-						$name .= ucfirst($piece);
-					}
-
-					return $name;
-				}
-			}
-		}
-
-		return $prettyName;
-
+		$extra = $package->getExtra();
+		return (isset($extra['module'])) ? $extra['module'] : array();
 	}
 }
